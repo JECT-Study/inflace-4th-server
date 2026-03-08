38391c1e03d4e3f34e17a0250542563bbfd63944
HwangRock <peter9244@naver.com>
2026-03-08 22:51:08 +0900

    chore: 프로젝트 세팅

---
+++ src/test/java/com/example/inflace/InflaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
