e4645404d51e14b580f997e23e0655610b0d1341
HwangRock <peter9244@naver.com>
2026-03-17 23:06:14 +0900

    refactor: 명세용 인터페이스 분리

--- src/main/java/com/example/inflace/global/controller/HealthCheckController.java
+++ src/main/java/com/example/inflace/global/controller/HealthCheckController.java
@@ -1,10 +1,7 @@
 package com.example.inflace.global.controller;
 
-import com.example.inflace.global.exception.ApiErrorDefines;
 import com.example.inflace.global.exception.ApiException;
 import com.example.inflace.global.exception.ErrorDefine;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +9,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Tag(name = "health check", description = "헬스 체크용 API")
-public class HealthCheckController {
+public class HealthCheckController implements HealthCheckApi {
 
 
-    @Operation(
-            summary = "헬스 체크",
-            description = "서버의 상태를 체크합니다. " +
-                    "헤더에 인증을 넣으면 예외를 반환합니다."
-    )
-    @ApiErrorDefines(ErrorDefine.INVALID_HEADER_ERROR)
     @GetMapping("/health-check")
     public ResponseEntity<Void> healthCheck(
             @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String authorization) {
