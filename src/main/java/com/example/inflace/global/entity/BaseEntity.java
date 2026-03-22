54a0d9b8b24f47fbc46d0f4a488f8002d6c9ca0e
sunJ0120 <sspure1214@gmail.com>
2026-03-22 18:01:07 +0900

    fix: baseEntity에 getter 추가

--- src/main/java/com/example/inflace/global/entity/BaseEntity.java
+++ src/main/java/com/example/inflace/global/entity/BaseEntity.java
@@ -3,12 +3,14 @@ package com.example.inflace.global.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {  // 직접 인스턴스화 방지를 위해 추상 클래스 설정
