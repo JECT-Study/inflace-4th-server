a41a2a7786a7ba0f1fe84a61fe02b78841f25b33
sunJ0120 <sspure1214@gmail.com>
2026-03-28 21:54:21 +0900

    fix: 필요없는 어노테이션 제거

--- src/main/java/com/example/inflace/domain/video/repository/VideoRepository.java
+++ src/main/java/com/example/inflace/domain/video/repository/VideoRepository.java
@@ -2,8 +2,6 @@ package com.example.inflace.domain.video.repository;
 
 import com.example.inflace.domain.video.domain.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
 }
--- src/main/java/com/example/inflace/domain/video/repository/VideoStatsRepository.java
+++ src/main/java/com/example/inflace/domain/video/repository/VideoStatsRepository.java
@@ -2,11 +2,8 @@ package com.example.inflace.domain.video.repository;
 
 import com.example.inflace.domain.video.domain.VideoStats;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface VideoStatsRepository extends JpaRepository<VideoStats, Long> {
     Optional<VideoStats> findByVideoId(Long videoId);
 }
