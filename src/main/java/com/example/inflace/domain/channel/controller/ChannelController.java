848caab60a11a3b9792ba34d1da09c9cbf93709e
youcastle03 <luckthb325@naver.com>
2026-03-28 18:32:01 +0900

    feat: dto&controller 우선 구현

--- src/main/java/com/example/inflace/domain/channel/controller/ChannelController.java
+++ src/main/java/com/example/inflace/domain/channel/controller/ChannelController.java
@@ -1,7 +1,14 @@
 package com.example.inflace.domain.channel.controller;
 
 import com.example.inflace.domain.channel.service.ChannelService;
+import com.example.inflace.domain.video.dto.ChannelTopVideosResponse;
+import com.example.inflace.global.exception.ApiErrorDefines;
+import com.example.inflace.global.exception.ErrorDefine;
+import com.example.inflace.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChannelController {
 
     private final ChannelService channelService;
+
+    @GetMapping("/{channelId}/tops")
+    public BaseResponse<ChannelTopVideosResponse> getTopVideos(
+            @PathVariable Long channelId,
+            @RequestParam(defaultValue = "LONG_FORM") String contentType
+    ) {
+        return new BaseResponse<>(channelService.getTopVideos(channelId, contentType));
+    }
 }
