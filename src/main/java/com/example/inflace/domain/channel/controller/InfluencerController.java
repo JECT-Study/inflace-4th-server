77544e4a415f07ce12f600df4158390b90398f22
HanYoonSoo <hdbstn3055@gmail.com>
2026-04-28 11:42:05 +0900

    feat: 인플루언서 검색 API를 추가

---
+++ src/main/java/com/example/inflace/domain/channel/controller/InfluencerController.java
@@ -0,0 +1,33 @@
+package com.example.inflace.domain.channel.controller;
+
+import com.example.inflace.domain.channel.dto.request.InfluencerSearchCondition;
+import com.example.inflace.domain.channel.dto.response.InfluencerSearchResponse;
+import com.example.inflace.domain.channel.service.InfluencerService;
+import com.example.inflace.global.response.BaseResponse;
+import com.example.inflace.global.response.SliceResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/influencers")
+@RequiredArgsConstructor
+public class InfluencerController {
+
+    private final InfluencerService influencerService;
+
+    @GetMapping
+    public BaseResponse<SliceResponse<InfluencerSearchResponse>> getInfluencersWithSearchCondition(
+            @ModelAttribute InfluencerSearchCondition searchCondition
+    ) {
+        return new BaseResponse<>(
+                SliceResponse.from(
+                        influencerService.getInfluencersWithSearchCondition(searchCondition),
+                        searchCondition.sortCriteriaEnum().value(),
+                        searchCondition.sortOrder().name()
+                )
+        );
+    }
+}
